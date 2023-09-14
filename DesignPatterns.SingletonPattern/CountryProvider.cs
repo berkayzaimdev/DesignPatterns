e114b98b6285c6f9a28e2edce7fb1025c6f18838
berkayzaimdev <berkayzaimdev@gmail.com>
2023-09-14 14:26:01 +0300

    Added: Singleton Pattern

---
+++ DesignPatterns.SingletonPattern/CountryProvider.cs
@@ -0,0 +1,36 @@
+﻿using DesignPatterns.SingletonPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SingletonPattern
+{
+    public class CountryProvider
+    {
+        private static CountryProvider instance = null;
+        public static CountryProvider Instance => instance ?? (instance = new CountryProvider());
+            // public static metot, Singleton Pattern'in ana düşüncesini en iyi şekilde yansıtan özelliğidir.
+            // Dışarıdan erişime açık olan bu metot ile, her seferinde aynı nesneyi döneceğimiz durumu garanti altına alırız.
+            // Eğer metodu ilk kez çağırıyorsak, yeni nesne oluşacaktır. Metodu tekrar çağırdığımızda ise zaten var olan nesne dönecektir.
+
+        private new List<Country> Countries { get; set; }
+
+        private CountryProvider() 
+            // private Constructor sayesinde birden fazla sayıda nesne oluşma ihtimalini sıfırlarız.
+        {
+            Task.Delay(2000).GetAwaiter().GetResult();
+
+            Countries = new List<Country>()
+            {
+                new Country(){ Name = "Türkiye" },
+                new Country(){ Name = "Birleşik Krallık" }
+            };
+        }
+
+        public int CountryCount => Countries.Count;
+
+        public async Task<List<Country>> GetCountries() => Countries;
+    }
+}
---
+++ DesignPatterns.SingletonPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿#region Avantajları
+// Global olarak erişim sağlayabiliriz.
+// Sadece ihtiyaç olduğu zaman oluştururuz.
+#endregion
+
+#region Dezavantajları
+// Singleton genellikle kötü bir tasarım kalıbı olarak kabul görmüştür.
+// UnitTest'in zorluğu
+// Dependency Injection'ın zorluğu
+#endregion
+
+using DesignPatterns.SingletonPattern;
+
+var countries1 = await CountryProvider.Instance.GetCountries();
+
+Console.WriteLine(CountryProvider.Instance.CountryCount);
+
+foreach(var country in countries1)
+    Console.WriteLine(country.Name);
