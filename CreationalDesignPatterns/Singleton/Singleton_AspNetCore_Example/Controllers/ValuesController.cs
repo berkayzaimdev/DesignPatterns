40db61875d67faa84cc5f6d4000ee102c4f38a14
Berkay Zaim <berkayzaimdev@gmail.com>
2025-01-13 23:40:48 +0300

    added singleton asp.net example

---
+++ CreationalDesignPatterns/Singleton/Singleton_AspNetCore_Example/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Singleton_AspNetCore_Example.Services;
+
+namespace Singleton_AspNetCore_Example.Controllers
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult X()
+        {
+            DatabaseService databaseService = DatabaseService.Instance;
+            databaseService.Connection();
+            databaseService.Disconnection();
+            return Ok();
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Y()
+        {
+            DatabaseService databaseService = DatabaseService.Instance;
+            databaseService.Connection();
+            databaseService.Disconnection();
+            return Ok();
+        }
+    }
+}
---
+++ CreationalDesignPatterns/Singleton/Singleton_AspNetCore_Example/Services/DatabaseService.cs
@@ -0,0 +1,40 @@
+﻿namespace Singleton_AspNetCore_Example.Services
+{
+    public class DatabaseService
+    {
+        DatabaseService()
+        {
+            Console.WriteLine($"{nameof(DatabaseService)} is created");
+        }
+
+        static DatabaseService databaseService;
+
+        public static DatabaseService Instance 
+        { 
+            get
+            {
+                if (databaseService == null)
+                {
+                    databaseService = new DatabaseService();
+                }
+                return databaseService;
+            } 
+        }
+
+        public int Count { get; set; }
+
+        public bool Connection()
+        {
+            Console.WriteLine($"bağlantı sağlandı");
+            Count++;
+            return true;
+        }
+
+        public bool Disconnection()
+        {
+            Console.WriteLine($"bağlantı kesildi");
+
+            return true;
+        }
+    }
+}
